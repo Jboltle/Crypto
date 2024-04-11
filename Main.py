1e9650ebee7e13a2c1b38c01ca9a14c758bb0883
unknown <stacbolt2@gmail.com>
2024-04-11 16:30:18 -0500

    Added the api data to the json file
    finally have the script wortking properly

--- Main.py
+++ Main.py
@@ -23,8 +23,7 @@ class Main:
 
         # Check for Cloudflare security challenge
         
-                    
-            # Find all elements with the specified class
+        # Find all elements with the specified class
         elements = driver.find_elements(By.XPATH, "//a[contains(@class, 'ds-dex-table-row-new')]")
 
         # List to store href links
@@ -36,6 +35,7 @@ class Main:
             hrefs.append(href)
 
         # Iterate over the collected href links to open them
+        pairs = []
         for i, href in enumerate(hrefs):
             print(f"Opening href {i + 1}: {href}")
             driver.get(href)
@@ -45,32 +45,37 @@ class Main:
                 # Get the copied pair
                 url = driver.current_url
                 cryptoUrlKey = url.split("/")[-1]
-                print("PairKEY:", cryptoUrlKey)
                 # Add the copied pair to the list
-                pairs = []
                 pairs.append(cryptoUrlKey)
-                self.save_to_json(pairs)    
             except IndexError:
                 print("No Pair Key")
         
         
         driver.quit()
 
-        # Save pairs to JSON file
+        # Call cryptoScreen method to get JSON data for each crypto URL and save to crypto.json
+        self.cryptoScreen(pairs)
 
     def save_to_json(self, cryptoUrlKey):
-        with open('crypto.json', 'w') as file:
+        with open('crypto.json', 'a') as file:
             json.dump(cryptoUrlKey, file, indent=4)
-        print('{cryptoUrlKey} saved to crypto.json')
+     
 
-    def cryptoScreen(self, cryptoUrlKey):
-        url = 'https://api.dexscreener.com/latest/dex/pairs/solana/' + cryptoUrlKey
-        getData = requests.get(url)
-        if getData.status_code == 200:
-            print(f"Status Code: {getData.status_code}, Content: {getData.json()}")
-        else:
-            print(f"Error handling request {getData.status_code}")
+    def cryptoScreen(self, cryptoUrls):
+        crypto_data = {}
+        for cryptoUrl in cryptoUrls:
+            url = 'https://api.dexscreener.com/latest/dex/pairs/solana/' + cryptoUrl
+            getData = requests.get(url)
+            if getData.status_code == 200:
+                print(f"Status Code: {getData.status_code}, Content: {getData.json()}")
+                crypto_data[cryptoUrl] = getData.json()
+            else:
+                print(f"Error handling request {getData.status_code}")
 
+        # Save crypto data to JSON file
+        with open('crypto.json', 'a') as file:
+            json.dump(crypto_data, file, indent=4)
+        print("Crypto data saved to 'crypto.json'.")
 
 # Instantiate Main class
 main = Main()
